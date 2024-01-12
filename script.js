ece4c83429fd64f21f9d33fdc52108bc72956b33
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-01-12 12:22:40 +0530

    Update header styling and add navigation links

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("this is our first script")
+// Wait for the Ionicons library to load
+document.addEventListener('DOMContentLoaded', function () {
+    // get element by id
+    let toggleBtn = document.getElementById('dark-mode-btn');
+
+    // add event listener
+    toggleBtn.addEventListener('click', function () {
+        // toggle the icon name
+        toggleBtn.setAttribute('name', toggleBtn.getAttribute('name') === 'sunny-outline' ? 'moon-outline' : 'sunny-outline');
+
+        // change theme
+        document.body.classList.toggle('dark-theme');
+    });
+});
